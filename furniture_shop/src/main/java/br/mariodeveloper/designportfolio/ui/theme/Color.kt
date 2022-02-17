69634cf4d9eb842960fc6c223e8ed369dfdd6b20
Mário Henrique <marioh90@gmail.com>
2022-02-17 18:48:43 -0300

    First Commit - Projeto Base

---
+++ furniture_shop/src/main/java/br/mariodeveloper/designportfolio/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package br.mariodeveloper.designportfolio.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+object MyColors {
+	val Furniture_Blue = Color(0xFF497DCB)
+	val Furniture_Black = Color(0xFF2d2c2c)
+	val Furniture_Yellow = Color(0xFFffce31)
+}
