0a976b4bd17eb3627a4c2bacc12f923bf4fb0331
Mário Henrique <marioh90@gmail.com>
2022-02-18 17:20:56 -0300

    * adicionado pasta de estilos  * Bibliotecas:   - Kotlin: 1.5.21 -> 1.6.10

--- furniture_shop/src/main/java/br/mariodeveloper/designportfolio/FurnitureActivity.kt
+++ furniture_shop/src/main/java/br/mariodeveloper/designportfolio/FurnitureActivity.kt
@@ -4,24 +4,35 @@ import android.os.Bundle
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 import br.mariodeveloper.designportfolio.ui.theme.DesignPortfolioTheme
 
-class MainActivity : ComponentActivity() {
+/**
+ * Created on 17/02/2022
+ */
+class FurnitureActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
 			DesignPortfolioTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-					Greeting("Android")
+					FurnitureScreen("Alberto")
 				}
 			}
 		}
@@ -29,14 +40,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun FurnitureScreen(name: String) {
+	Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween) {
+		Row {
+			Image(painter = painterResource(R.drawable.ic_launcher_background), contentDescription = "")
+			Text("Hi,\n $name")
+		}
+
+		Icon(Icons.Filled.ShoppingCart, contentDescription = "shopping icon")
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
 	DesignPortfolioTheme {
-		Greeting("Android")
+		FurnitureScreen("Android")
 	}
 }
